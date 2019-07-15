352b6fc44ed95f0547225b8c9e684f5c4abece5a
KijkEr <joep_is_cool@hotmail.com>
2019-07-15 14:34:59 +0200

    Initial commit

---
+++ Code/main.py
@@ -0,0 +1,35 @@
+import smtplib, ssl, secrets, urllib.request
+
+port = 465
+password = secrets.password
+
+context = ssl.create_default_context()
+
+sender_email = "mailtestbart@gmail.com"
+receiver_email = "joep_is_cool@hotmail.com"
+
+external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+
+path = 'ip.txt'
+ip_file_r = open(path, 'r')
+ip_check = ip_file_r.read()
+
+print(ip_check)
+print(external_ip)
+
+ip_file_r.close()
+
+message = """\
+Subject: IP
+
+Extern IP adres is: %s""" % external_ip
+
+if external_ip != ip_check:
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login("mailtestbart@gmail.com", password)
+        server.sendmail(sender_email, receiver_email, message)
+
+    ip_file_w = open(path, 'w')
+    ip_file_w.write(external_ip)
+    ip_file_w.close()
